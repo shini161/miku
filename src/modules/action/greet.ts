9440f0c6f114e054e4589ef4dbd27870b4b18344
shini161 <85759040+shini161@users.noreply.github.com>
2022-05-22 23:11:37 +0200

    greet command added

---
+++ src/modules/action/greet.ts
@@ -0,0 +1,52 @@
+import { Command } from "../../structures/Command";
+import ActionData from "../../assets/action-module.json";
+import Colors from "../../assets/colors.json";
+import { ColorResolvable } from "discord.js";
+
+const name = "greet"; // command name
+export default new Command({
+  name,
+  usages: `%PREFIX%${name} <@user>`,
+  required: true,
+
+  run: async ({ client, message, args }) => {
+    let text: string[];
+    let images: string[] = ActionData[name].images;
+    const color = Colors.celestialBlue;
+    const target = message.mentions.members.first();
+
+    switch (target?.id) {
+      case undefined:
+        return message.reply({
+          content: "Please mention a user!",
+        });
+        break;
+      case message.author.id:
+        text = [`${message.author.username}, that's sad...`];
+        return sendEmbed(text, images);
+        break;
+      default:
+        text = [
+          `${message.author.username} says hi to ${target.user.username}.`,
+        ];
+        return sendEmbed(text, images);
+    }
+
+    function sendEmbed(text: string[], images: string[]) {
+      const embed = {
+        author: {
+          name: text[Math.floor(Math.random() * text.length)],
+          icon_url: message.author.displayAvatarURL({ dynamic: true }),
+        },
+        image: {
+          url: images[Math.floor(Math.random() * images.length)],
+        },
+        color: color as ColorResolvable,
+      };
+
+      message.channel.send({
+        embeds: [embed],
+      });
+    }
+  },
+});
