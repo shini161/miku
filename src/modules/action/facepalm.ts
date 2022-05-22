4a5da0b3eb251654b7bc475ee11b945a7330fa71
shini161 <85759040+shini161@users.noreply.github.com>
2022-05-22 23:09:10 +0200

    added
    facepalm command added

---
+++ src/modules/action/facepalm.ts
@@ -0,0 +1,51 @@
+import { Command } from "../../structures/Command";
+import ActionData from "../../assets/action-module.json";
+import Colors from "../../assets/colors.json";
+import { ColorResolvable } from "discord.js";
+
+const name = "facepalm"; // command name
+export default new Command({
+  name,
+  usages: `%PREFIX%${name} [@user]`,
+  required: true,
+
+  run: async ({ client, message, args }) => {
+    let text: string[];
+    let images: string[] = ActionData[name].images;
+    const color = Colors.celestialBlue;
+    const target = message.mentions.members.first();
+
+    switch (target?.id) {
+      case undefined:
+        text = [`${message.author.username} is facepalming.`];
+        return sendEmbed(text, images);
+        break;
+      case message.author.id:
+        text = [`${message.author.username} is facepalming.`];
+        return sendEmbed(text, images);
+        break;
+      default:
+        text = [
+          `${message.author.username} is facepalming at ${target.user.username}.`,
+        ];
+        return sendEmbed(text, images);
+    }
+
+    function sendEmbed(text: string[], images: string[]) {
+      const embed = {
+        author: {
+          name: text[Math.floor(Math.random() * text.length)],
+          icon_url: message.author.displayAvatarURL({ dynamic: true }),
+        },
+        image: {
+          url: images[Math.floor(Math.random() * images.length)],
+        },
+        color: color as ColorResolvable,
+      };
+
+      message.channel.send({
+        embeds: [embed],
+      });
+    }
+  },
+});
