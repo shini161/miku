e69f5cce9f807be00520e0d4559ff45ed15414d8
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:28:57 +0200

    updated

--- src/events/mentionPrefix.ts
+++ src/events/mentionPrefix.ts
@@ -1,34 +1,30 @@
 import { Event } from "../structures/Event";
 import { client } from "..";
 import getPrefix from "../utils/getPrefix";
-import { GuildChannelResolvable, Permissions } from "discord.js";
-import config from "../assets/config.json";
+import { Permissions } from "discord.js";
+import config from "../config.json";
 
-export default new Event("messageCreate", (message) => {
+export default new Event("messageCreate", async (message) => {
   const clientId = client.user.id;
   if (
-    message.content !== `<@${clientId}>` &&
-    message.content !== `<@!${clientId}>`
+      message.content !== `<@${clientId}>` &&
+      message.content !== `<@!${clientId}>`
   )
     return;
-  if (message.channel.type === "DM") return returnPrefix("USER");
-  if (
-    !message.guild.me
-      .permissionsIn(message.channel)
-      .has(Permissions.FLAGS.SEND_MESSAGES) &&
-    !message.guild.me.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
-  )
-    return;
-  returnPrefix("GUILD");
-
-  async function returnPrefix(type: "USER" | "GUILD") {
-    if (type === "USER")
-      return message.reply({
-        content: `My prefix is **${config.prefix}**`,
-      });
-    const prefix = await getPrefix(message.guild.id);
+  if (message.channel.type === "DM")
     return message.reply({
-      content: `My prefix in this guild is **${prefix}**`,
+      content: `My prefix is **${config.prefix}**`,
     });
-  }
+
+  if (
+      !message.guild.me
+          .permissionsIn(message.channel)
+          .has(Permissions.FLAGS.SEND_MESSAGES) &&
+      !message.guild.me.permissions.has(Permissions.FLAGS.ADMINISTRATOR)
+  )
+    return;
+  const prefix = await getPrefix(message.guildId);
+  return message.reply({
+    content: `My prefix in this guild is **${prefix}**`,
+  });
 });
