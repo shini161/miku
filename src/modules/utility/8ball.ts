73f0f8bfed9012d8a77ecc16f3a0a59f13329bf7
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:31:58 +0200

    added

---
+++ src/modules/utility/8ball.ts
@@ -0,0 +1,21 @@
+import { Command } from "../../structures/Command";
+import replies from "../../../assets/8ball.json";
+
+export default new Command({
+    name: "8ball",
+    usages: "$PREFIX$8ball <text>",
+    channel_type: "ALL",
+
+    run: async ({ client, message, args }) => {
+        if (!args[0])
+            return message.reply({
+                content: "❌ Missing arguments!",
+            });
+
+        const reply = replies[Math.floor(Math.random() * replies.length)];
+
+        await message.reply({
+            content: reply,
+        });
+    },
+});
