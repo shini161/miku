b2990455714cb640c39de4cde5d569945666d82a
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:34:05 +0200

    added

---
+++ src/utils/getMuterole.ts
@@ -0,0 +1,16 @@
+import sql from "../structures/Database";
+import addGuild from "./addGuild";
+import { client } from "..";
+
+export default async function getMuterole(guild_id: string) {
+  const res = await sql`SELECT * FROM guilds WHERE id = ${guild_id}`;
+  if (!res.length) await addGuild(guild_id);
+  if (res[0].muterole) {
+    const guild = client.guilds.cache.get(guild_id);
+    if (guild) {
+      if (guild.roles.cache.has(res[0].muterole)) return res[0].muterole;
+      await sql`UPDATE guilds SET muterole = NULL WHERE id = ${guild_id}`;
+    }
+  }
+  return null;
+}
