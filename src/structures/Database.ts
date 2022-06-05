c5eb72e1c17cf4bb61ba9685292d2ae3c6a40079
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:33:33 +0200

    updated

--- src/structures/Database.ts
+++ src/structures/Database.ts
@@ -1,10 +1,11 @@
-import { Client } from "pg";
-
-export const psql = new Client({
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-  port: +process.env.DBPORT,
-  password: process.env.DBPASS,
-  database: process.env.DBNAME,
-  connectionTimeoutMillis: +process.env.DBTIMEOUT,
+import postgres from "postgres";
+export default postgres({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: +process.env.DB_PORT,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  connect_timeout: 10,
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
 });
