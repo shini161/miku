8a66366586dc7fd74758f42eb4c72131330a6623
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:34:41 +0200

    added

---
+++ src/typings/SlashCommand.ts
@@ -0,0 +1,27 @@
+import {
+  ChatInputApplicationCommandData,
+  CommandInteraction,
+  CommandInteractionOptionResolver,
+  GuildMember,
+  PermissionResolvable,
+} from "discord.js";
+import { ExtendedClient } from "../structures/Client";
+
+export interface ExtendedInteraction extends CommandInteraction {
+  member: GuildMember;
+}
+
+interface RunOptions {
+  client: ExtendedClient;
+  interaction: ExtendedInteraction;
+  args: CommandInteractionOptionResolver;
+}
+
+type RunFunction = (options: RunOptions) => any;
+
+export type SlashCommandType = {
+  userPermissions?: PermissionResolvable[];
+  cooldown?: number | "Module";
+  channel_type?: "GUILD_ONLY" | "DM_ONLY" | "ALL"
+  run: RunFunction;
+} & ChatInputApplicationCommandData;
