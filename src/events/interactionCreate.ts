f300a47740d628d902e76a26ab40e44f38b88fe3
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:28:52 +0200

    updated

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,15 +1,44 @@
 import { CommandInteractionOptionResolver } from "discord.js";
 import { client } from "..";
 import { Event } from "../structures/Event";
-import { ExtendedInteraction } from "../typings/SlashCmds";
+import { ExtendedInteraction } from "../typings/SlashCommand";
+import config from "../config.json";
 
 export default new Event("interactionCreate", async (interaction) => {
   if (interaction.isCommand()) {
     await interaction.deferReply();
-    const command = client.slashCmds.get(interaction.commandName);
-    if (!command)
-      return interaction.followUp("You have used a non existent command");
 
+    const command = client.slashCmds.get(interaction.commandName);
+    const devCommand = client.devSlashCmds.get(interaction.commandName);
+
+    if (!command && !devCommand)
+      return interaction.followUp("That command doesn't exist!");
+
+    if (command) {
+      switch (command.channel_type) {
+        case "DM_ONLY":
+          if (interaction.guild)
+            return interaction.followUp({
+              content: "This command cannot be runned in guilds!",
+            });
+        case "GUILD_ONLY":
+          if (!interaction.guild)
+            return interaction.followUp({
+              content: "This command can be runned in guilds only!",
+            });
+        default:
+          command.run({
+            args: interaction.options as CommandInteractionOptionResolver,
+            client,
+            interaction: interaction as ExtendedInteraction,
+          });
+      }
+      return;
+    }
+
+    if (devCommand.ownerOnly) {
+      if (interaction.user.id !== config.ownerId) return;
+    }
     command.run({
       args: interaction.options as CommandInteractionOptionResolver,
       client,
