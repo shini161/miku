11b69447989fbea453146c59be358a8c294d79c1
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-05 20:34:58 +0200

    updated

--- src/utils/addGuild.ts
+++ src/utils/addGuild.ts
@@ -1,7 +1,11 @@
 import sql from "../structures/Database";
+import clientError from "./clientError";
+import { client } from "..";
 
-export default async function addGuild(id: string) {
-  const res = await sql`SELECT * FROM guilds WHERE id = ${id}`;
-  if (!res.length) await sql`INSERT INTO guilds (id) VALUES (${id})`;
-
+export default async function addGuild(guild_id: string) {
+  const inGuild = await client.guilds.cache.get(guild_id);
+  if (!inGuild)
+    throw new clientError("I must be in the guild to save it to the database!");
+  const res = await sql`SELECT * FROM guilds WHERE id = ${guild_id}`;
+  if (!res.length) await sql`INSERT INTO guilds (id) VALUES (${guild_id})`;
 }
