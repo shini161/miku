51074f34cf7edb6f262a705f61cb79be13853515
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-06 18:54:20 +0200

    added smug command

--- src/slashCmds/action.ts
+++ src/slashCmds/action.ts
@@ -1131,6 +1131,23 @@ export default new SlashCommand({
             await sendEmbed(text, images);
         }
         break;
+      case "smug":
+        switch (target?.id) {
+          case undefined:
+            text = [`${interaction.user.username} has a smug look.`];
+            await sendEmbed(text, images);
+            break;
+          case interaction.user.id:
+            text = [`${interaction.user.username} has a smug look.`];
+            await sendEmbed(text, images);
+            break;
+          default:
+            text = [
+              `${interaction.user.username} smugs at ${target.username}.`,
+            ];
+            await sendEmbed(text, images);
+        }
+        break;
       default:
         await interaction.followUp({
           content: "❌ Sorry, an error has occurred!",
