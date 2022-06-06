ff6f0be2eaf585b4ada9f876de7c1901df990b0a
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-06 18:34:22 +0200

    added greet command

--- src/slashCmds/action.ts
+++ src/slashCmds/action.ts
@@ -719,6 +719,19 @@ export default new SlashCommand({
             await sendEmbed(text, images);
         }
         break;
+      case "greet":
+        switch (target?.id) {
+          case interaction.user.id:
+            text = [`${interaction.user.username}, that's sad...`];
+            await sendEmbed(text, images);
+            break;
+          default:
+            text = [
+              `${interaction.user.username} says hi to ${target.username}.`,
+            ];
+            await sendEmbed(text, images);
+        }
+        break;
       default:
         await interaction.followUp({
           content: "❌ Sorry, an error has occurred!",
