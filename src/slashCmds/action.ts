9779f4465f3499021b7d43876dcca9213f9016cd
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-06 18:58:23 +0200

    added yes command

--- src/slashCmds/action.ts
+++ src/slashCmds/action.ts
@@ -1244,6 +1244,23 @@ export default new SlashCommand({
             await sendEmbed(text, images);
         }
         break;
+      case "yes":
+        switch (target?.id) {
+          case undefined:
+            text = [`${interaction.user.username} is agreeing.`];
+            await sendEmbed(text, images);
+            break;
+          case interaction.user.id:
+            text = [`${interaction.user.username} is agreeing.`];
+            await sendEmbed(text, images);
+            break;
+          default:
+            text = [
+              `${interaction.user.username} agrees with ${target.username}.`,
+            ];
+            await sendEmbed(text, images);
+        }
+        break;
       default:
         await interaction.followUp({
           content: "❌ Sorry, an error has occurred!",
