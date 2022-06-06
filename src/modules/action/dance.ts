c566f005368f1d770ef05511816ef81e9509e47d
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-06 18:15:21 +0200

    updated

--- src/modules/action/dance.ts
+++ src/modules/action/dance.ts
@@ -19,20 +19,20 @@ export default new Command({
     switch (target?.id) {
       case undefined:
         text = [`${message.author.username} is dancing!`];
-        sendEmbed(text, images);
+        await sendEmbed(text, images);
         break;
       case message.author.id:
         text = [`${message.author.username} is dancing!`];
-        sendEmbed(text, images);
+        await sendEmbed(text, images);
         break;
       default:
         text = [
           `${message.author.username} is dancing with ${target.user.username}!`,
         ];
-        sendEmbed(text, images);
+        await sendEmbed(text, images);
     }
 
-    function sendEmbed(text: string[], images: string[]) {
+    async function sendEmbed(text: string[], images: string[]) {
       const embed = {
         author: {
           name: text[Math.floor(Math.random() * text.length)],
