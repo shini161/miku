39043601aa4fb4efbb512e5769c5fe85de336927
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-06 18:41:52 +0200

    added "no" command

--- src/slashCmds/action.ts
+++ src/slashCmds/action.ts
@@ -878,6 +878,23 @@ export default new SlashCommand({
             await sendEmbed(text, images);
         }
         break;
+      case "no":
+        switch (target?.id) {
+          case undefined:
+            text = [`${interaction.user.username} disagrees.`];
+            await sendEmbed(text, images);
+            break;
+          case interaction.user.id:
+            text = [`${interaction.user.username} disagrees.`];
+            await sendEmbed(text, images);
+            break;
+          default:
+            text = [
+              `${interaction.user.username} disagrees with ${target.username}.`,
+            ];
+            await sendEmbed(text, images);
+        }
+        break;
       default:
         await interaction.followUp({
           content: "❌ Sorry, an error has occurred!",
