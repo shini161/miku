c11a93847c44f45c33c5f5dcedcf77dfe160e8d0
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-07 21:12:20 +0200

    updated

--- src/utils/getLang-guild.ts
+++ src/utils/getLang-guild.ts
@@ -1,10 +1,11 @@
 import sql from "../structures/Database";
 import config from "../config.json";
 import addGuild from "./addGuild";
+import { langsList } from "../../assets/langs/langs";
 
-export default async function getLangGuild(guild_id) {
+export default async function getLangGuild(guild_id): Promise<langsList> {
   const res = await sql`SELECT * FROM guilds WHERE id = ${guild_id}`;
   if (!res.length) await addGuild(guild_id);
   if (res[0].language) return res[0].language;
-  return config.default_lang;
+  return config.default_lang as langsList;
 }
