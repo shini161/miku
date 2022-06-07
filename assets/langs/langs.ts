9fb0b66fea33a02af58219e627f4bb95ca9aca41
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-07 21:13:01 +0200

    added

---
+++ assets/langs/langs.ts
@@ -0,0 +1,23 @@
+import en_US from "./en_US";
+import es_ES from "./es_ES";
+import fr_FR from "./fr_FR";
+import hi_IN from "./hi_IN";
+import it_IT from "./it_IT";
+import jp_JP from "./jp_JP";
+
+export type langsList =
+  | "en_US"
+  | "es_ES"
+  | "fr_FR"
+  | "hi_IN"
+  | "it_IT"
+  | "jp_JP";
+
+export default {
+  en_US,
+  es_ES,
+  fr_FR,
+  hi_IN,
+  it_IT,
+  jp_JP,
+};
