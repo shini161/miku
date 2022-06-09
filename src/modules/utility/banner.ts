2048b75a164659579dc8a069fd7ef3884d40fcf9
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-09 22:04:16 +0200

    updated

--- src/modules/utility/banner.ts
+++ src/modules/utility/banner.ts
@@ -1,82 +1,71 @@
 import { Command } from "../../structures/Command";
 import { ColorResolvable } from "discord.js";
 import Colors from "../../../assets/colors.json";
+import getLangRelative from "../../utils/getLang-relative";
+import langs from "../../../assets/langs/langs";
+import { getUserId } from "./getStringID";
+import reactOnDM from "../../utils/reactOnDM";
 
 export default new Command({
   name: "banner",
   usages: "$PREFIX$banner [@user]\n$PREFIX$banner [userID]",
-  required: true,
+  channel_type: "ALL",
+  cooldown: 1000 * 8,
+  required: false,
 
   run: async ({ client, message, args }) => {
-    try {
-      const color = Colors.celestialBlue;
-      let id = message.author.id;
+    const color = Colors.celestialBlue;
+    const lang = await getLangRelative(message?.guildId, message.author.id);
+    const link_as = langs[lang].common.link_as;
+    let id = getUserId(args?.[0], message.author.id);
 
-      if (args[0]) {
-        if (
-          args[0].startsWith("<@") &&
-          args[0].endsWith(">") &&
-          args[0].length == 21 &&
-          !isNaN(+args[0].slice(2, 20))
-        )
-          id = args[0].slice(2, 20);
-        if (
-          args[0].startsWith("<@!") &&
-          args[0].endsWith(">") &&
-          args[0].length == 22 &&
-          !isNaN(+args[0].slice(3, 21))
-        )
-          id = args[0].slice(3, 21);
-        if (!isNaN(+args[0]) && args[0].length === 18) id = args[0];
-      }
+    getBanner(id);
+    function getBanner(id: string) {
+      client.users
+        .fetch(id)
+        .then(async (user) => {
+          const target = await user.fetch();
+          const png = target.bannerURL({ format: "png" });
+          const jpg = target.bannerURL({ format: "jpg" });
+          const webp = target.bannerURL({ format: "webp" });
 
-      getBanner(id);
-      function getBanner(id: string) {
-        client.users
-          .fetch(id)
-          .then(async (user) => {
-            const target = await user.fetch();
-            const png = target.bannerURL({ format: "png" });
-            const jpg = target.bannerURL({ format: "jpg" });
-            const webp = target.bannerURL({ format: "webp" });
-
-            if (target.banner) {
-              const embed = {
-                author: {
-                  name: `${target.tag}`,
-                  icon_url: target.displayAvatarURL({ dynamic: true }),
-                },
-                title: "Banner",
-                description: `**Link as:\n[png](${png}) | [jpg](${jpg}) | [webp](${webp})**`,
-                image: {
-                  url: target.bannerURL({ dynamic: true, size: 4096 }),
-                },
-                color: color as ColorResolvable,
-              };
-              return message.channel.send({
-                embeds: [embed],
-              });
-            }
+          if (target.banner) {
             const embed = {
               author: {
                 name: `${target.tag}`,
                 icon_url: target.displayAvatarURL({ dynamic: true }),
               },
-              description: "That user doesn't have a banner.",
+              title: "Banner",
+              description: `**${link_as}:\n[png](${png}) | [jpg](${jpg}) | [webp](${webp})**`,
+              image: {
+                url: target.bannerURL({ dynamic: true, size: 4096 }),
+              },
               color: color as ColorResolvable,
             };
-            message.channel.send({
+            return message.channel.send({
               embeds: [embed],
             });
-          })
-          .catch(() => {
-            return message.reply({
-              content: "I couldn't find that user!",
-            });
+          }
+          const embed = {
+            author: {
+              name: `${target.tag}`,
+              icon_url: target.displayAvatarURL({ dynamic: true }),
+            },
+            description: langs[lang].modules.utility.banner.no_banner,
+            color: color as ColorResolvable,
+          };
+          if (message.guild) {
+            return reactOnDM(message, { embeds: [embed] });
+          }
+          return message.channel.send({
+            embeds: [embed],
           });
-      }
-    } catch (err) {
-      console.log(err);
+        })
+        .catch(() => {
+          return message.reply({
+            content: langs[lang].common.user_not_found,
+          });
+        });
     }
   },
 });
