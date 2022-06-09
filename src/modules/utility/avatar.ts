e4c1eb153392593839098ed20f09e481c67054f7
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-09 21:54:34 +0200

    updated

--- src/modules/utility/avatar.ts
+++ src/modules/utility/avatar.ts
@@ -1,68 +1,58 @@
 import { Command } from "../../structures/Command";
 import { ColorResolvable } from "discord.js";
 import Colors from "../../../assets/colors.json";
+import getLangRelative from "../../utils/getLang-relative";
+import langs from "../../../assets/langs/langs";
+import { getUserId } from "./getStringID";
+import reactOnDM from "../../utils/reactOnDM";
 
 export default new Command({
   name: "avatar",
   aliases: ["pic", "pfp", "av"],
   usages: "$PREFIX$avatar [@user]\n$PREFIX$avatar [userID]",
+  channel_type: "ALL",
+  cooldown: 1000 * 8,
+  required: false,
 
   run: async ({ client, message, args }) => {
-    try {
-      const color = Colors.celestialBlue;
-      let id = message.author.id;
+    const color = Colors.celestialBlue;
+    const lang = await getLangRelative(message?.guildId, message.author.id);
+    const link_as = langs[lang].common.link_as;
+    let id = getUserId(args?.[0], message.author.id);
 
-      if (args[0]) {
-        if (
-          args[0].startsWith("<@") &&
-          args[0].endsWith(">") &&
-          args[0].length == 21 &&
-          !isNaN(+args[0].slice(2, 20))
-        )
-          id = args[0].slice(2, 20);
-        if (
-          args[0].startsWith("<@!") &&
-          args[0].endsWith(">") &&
-          args[0].length == 22 &&
-          !isNaN(+args[0].slice(3, 21))
-        )
-          id = args[0].slice(3, 21);
-        if (!isNaN(+args[0]) && args[0].length === 18) id = args[0];
-      }
+    getAvatar(id);
+    function getAvatar(id: string) {
+      client.users
+        .fetch(id)
+        .then(async (user) => {
+          const png = user.displayAvatarURL({ format: "png" });
+          const jpg = user.displayAvatarURL({ format: "jpg" });
+          const webp = user.displayAvatarURL({ format: "webp" });
 
-      getAvatar(id);
-      function getAvatar(id: string) {
-        client.users
-          .fetch(id)
-          .then((user) => {
-            const png = user.displayAvatarURL({ format: "png" });
-            const jpg = user.displayAvatarURL({ format: "jpg" });
-            const webp = user.displayAvatarURL({ format: "webp" });
-
-            const embed = {
-              author: {
-                name: `${user.tag}`,
-                icon_url: user.displayAvatarURL({ dynamic: true }),
-              },
-              title: "Avatar",
-              description: `**Link as:\n[png](${png}) | [jpg](${jpg}) | [webp](${webp})**`,
-              image: {
-                url: user.displayAvatarURL({ dynamic: true, size: 512 }),
-              },
-              color: color as ColorResolvable,
-            };
-            message.channel.send({
-              embeds: [embed],
-            });
-          })
-          .catch(() => {
-            return message.reply({
-              content: "I couldn't find that user!",
-            });
+          const embed = {
+            author: {
+              name: `${user.tag}`,
+              icon_url: user.displayAvatarURL({ dynamic: true }),
+            },
+            title: "Avatar",
+            description: `**${link_as}:\n[png](${png}) | [jpg](${jpg}) | [webp](${webp})**`,
+            image: {
+              url: user.displayAvatarURL({ dynamic: true, size: 512 }),
+            },
+            color: color as ColorResolvable,
+          };
+          if (message.guild) {
+            return reactOnDM(message, { embeds: [embed] });
+          }
+          return message.channel.send({
+            embeds: [embed],
           });
-      }
-    } catch (err) {
-      console.log(err);
+        })
+        .catch(() => {
+          return message.reply({
+            content: langs[lang].common.user_not_found,
+          });
+        });
     }
   },
 });
