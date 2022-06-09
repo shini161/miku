577a7d6ffea132bf1e4153632f1230f2468afc38
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-09 17:24:27 +0200

    updated

--- src/modules/information/invite.ts
+++ src/modules/information/invite.ts
@@ -12,23 +12,24 @@ import langs from "../../../assets/langs/langs";
 export default new Command({
   name: "invite",
   usages: "$PREFIX$invite",
-  required: true,
+  channel_type: "ALL",
+  required: false,
 
   run: async ({ message }) => {
-    try {
-      const color = Colors.celestialBlue;
-      const lang = await getLangUser(message.author.id);
-      const { greenTick, redTick, greenTickCustom, redTickCustom } = Emojis;
-      const invite =
-        "https://discord.com/api/oauth2/authorize?client_id=841706730828333117&permissions=141636791750&scope=applications.commands%20bot";
+    const color = Colors.celestialBlue;
+    const lang = await getLangUser(message.author.id);
+    const { greenTick, redTick, greenTickCustom, redTickCustom } = Emojis;
+    const invite =
+      "https://discord.com/api/oauth2/authorize?client_id=841706730828333117&permissions=141636791750&scope=applications.commands%20bot";
 
-      const embed = {
-        title: langs[lang].modules.information.invite.embed.title,
-        description:
-          langs[lang].modules.information.invite.embed.description(invite),
-        color: color as ColorResolvable,
-      };
+    const embed = {
+      title: langs[lang].modules.information.invite.embed.title,
+      description:
+        langs[lang].modules.information.invite.embed.description(invite),
+      color: color as ColorResolvable,
+    };
 
+    if (message.guild) {
       if (
         message.guild.me
           .permissionsIn(message.channel as GuildChannelResolvable)
@@ -38,40 +39,45 @@ export default new Command({
         return message.reply({
           embeds: [embed],
         });
+      await sendDM();
+    } else
+      await message
+        .reply({
+          embeds: [embed],
+        })
+        .catch(() => {
+          return;
+        });
 
-      return sendDM();
-      function sendDM() {
-        let trigger = true;
-        message.author
-          .send({
-            embeds: [embed],
-          })
-          .catch(() => {
-            trigger = false;
-          });
+    function sendDM() {
+      let trigger = true;
+      message.author
+        .send({
+          embeds: [embed],
+        })
+        .catch(() => {
+          trigger = false;
+        });
+      if (
+        message.guild.me
+          .permissionsIn(message.channel as GuildChannelResolvable)
+          .has(Permissions.FLAGS.ADD_REACTIONS)
+      ) {
         if (
           message.guild.me
             .permissionsIn(message.channel as GuildChannelResolvable)
-            .has(Permissions.FLAGS.ADD_REACTIONS)
+            .has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS)
         ) {
-          if (
-            message.guild.me
-              .permissionsIn(message.channel as GuildChannelResolvable)
-              .has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS)
-          ) {
-            if (trigger) {
-              return message.react(greenTickCustom);
-            }
-            return message.react(redTickCustom);
-          }
           if (trigger) {
-            return message.react(greenTick);
+            return message.react(greenTickCustom);
           }
-          return message.react(redTick);
+          return message.react(redTickCustom);
         }
+        if (trigger) {
+          return message.react(greenTick);
+        }
+        return message.react(redTick);
       }
-    } catch (err) {
-      console.log(err);
     }
   },
 });
