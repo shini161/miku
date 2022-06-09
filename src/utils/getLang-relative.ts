d2d872489dc72d69c2610bde9d0e2f694561cd47
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-09 17:24:42 +0200

    fixed

--- src/utils/getLang-relative.ts
+++ src/utils/getLang-relative.ts
@@ -3,16 +3,20 @@ import config from "../config.json";
 import addGuild from "./addGuild";
 import getLangUser from "./getLang-user";
 import { langsList } from "../../assets/langs/langs";
+import getLangGuild from "./getLang-guild";
 
 export default async function getLangRelative(
   guild_id: string,
-  user_id
+  user_id: string
 ): Promise<langsList> {
-  const res = await sql`SELECT * FROM guilds WHERE id = ${guild_id}`;
-  if (!res.length) {
-    await addGuild(guild_id);
-    return await getLangUser(user_id);
+  if (guild_id) {
+    const res = await sql`SELECT * FROM guilds WHERE id = ${guild_id}`;
+    if (!res.length) {
+      await addGuild(guild_id);
+      return await getLangUser(user_id);
+    }
+    if (res[0]?.language) return res[0].language;
+    return config.default_lang as langsList;
   }
-  if (res[0]?.language) return res[0].language;
-  return config.default_lang as langsList;
+  return await getLangUser(user_id);
 }
