e39a61bec9606a8098979a11402e1b02b2ff5b8c
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-09 17:24:46 +0200

    fixed

--- src/utils/getPrefix.ts
+++ src/utils/getPrefix.ts
@@ -3,6 +3,7 @@ import config from "../config.json";
 import addGuild from "./addGuild";
 
 export default async function getPrefix(guild_id: string): Promise<string> {
+  if (!guild_id) return config.prefix;
   const res = await sql`SELECT * FROM guilds WHERE id = ${guild_id}`;
   if (!res.length) await addGuild(guild_id);
   if (res[0]?.prefix) return res[0].prefix;
