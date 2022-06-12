c919cd36f67ea57f3940b6d22938961bdf69de25
shini161 <85759040+shini161@users.noreply.github.com>
2022-06-12 12:25:33 +0200

    updated

--- src/modules/utility/random.ts
+++ src/modules/utility/random.ts
@@ -1,145 +1,124 @@
-/*
 import { Command } from "../../structures/Command";
 import { ColorResolvable } from "discord.js";
-import { get } from "request-promise-native";
 import Colors from "../../../assets/colors.json";
-import config from "../../config.json";
+import getLangRelative from "../../utils/getLang-relative";
+import langs from "../../../assets/langs/langs";
+import axios from "axios";
 
 export default new Command({
   name: "random",
   aliases: ["rand"],
   usages:
-    "$PREFIX$random <type>\n\n" +
-    "**Types**:\n- waifu/wa/w => Sends a random waifu image.\n" +
-    "neko/ne => Sends a random neko image.\n" +
-    "number/num/n => Sends a random number.",
-  required: true,
+    "$PREFIX$random <types>\n" +
+    "**Types**:\n- waifu/wa ~> to return a random waifu image.\n" +
+    "- neko\ne ~> to return a random neko image." +
+    "- number/numb ~> to return a random number.",
+  cooldown: 1000 * 4,
+  channel_type: "ALL",
+  required: false,
 
-  run: async ({ client, message, args }) => {
-    try {
-      const prefix = config.prefix;
-      const color = Colors.celestialBlue;
-      const query = args[0]?.toLowerCase();
+  run: async ({ message, args }) => {
+    const lang = await getLangRelative(message?.guildId, message.author.id);
+    const color = Colors.celestialBlue;
+    const query = args[0]?.toLowerCase();
 
-      const waifuArray = ["waifu", "wa", "w"];
-      const nekoArray = ["neko", "ne"];
-      const numberArray = ["number", "num", "n"];
+    const waifuKeys = ["waifu", "wa"];
+    const nekoKeys = ["neko", "ne"];
+    const numberKeys = ["number", "numb"];
 
-      const syntaxError = {
-        title: "Syntax Error",
-        fields: [
-          {
-            name: "Usages:",
-            value:
-              `${prefix}random <type> \n${prefix}rand <type> \n\n**Types**:` +
-              `\nwaifu/wa/w => Sends a random waifu image. \nneko/ne => Sends a random neko image.` +
-              `\nnumber/num/n => Sends a random number.`,
-          },
-        ],
-        color: Colors.syntaxError as ColorResolvable,
-      };
-
-      if (!query)
-        return message.reply({
-          embeds: [syntaxError],
-        });
-
-      if (waifuArray.includes(query)) {
-        const option = {
-          url: `https://neko-love.xyz/api/v1/waifu`,
-          method: "GET",
-          headers: {
-            "Content-type": "application/vnd.api+json",
-            Accept: "application/vnd.api+json",
-          },
-          json: true,
-        };
-        const image = await get(option)
-          .then((res) => res.url)
-          .catch(() => {
-            return message.reply({
-              content: "No results were found!",
-            });
-          });
-        if (!image)
-          return message.reply({
-            content: "No results were found!",
-          });
-
-        const embed = {
-          title: "Random Waifu",
-          image: {
-            url: image,
-          },
-          color: color as ColorResolvable,
-        };
-
-        return message.channel.send({
-          embeds: [embed],
-        });
-      }
-      if (nekoArray.includes(query)) {
-        const option = {
-          url: `https://nekos.life/api/neko`,
-          method: "GET",
-          headers: {
-            "Content-type": "application/vnd.api+json",
-            Accept: "application/vnd.api+json",
-          },
-          json: true,
-        };
-
-        const image = await get(option)
-          .then((res) => res.neko)
-          .catch(() => {
-            return message.reply({
-              content: "No results were found!",
-            });
-          });
-        if (!image)
-          return message.reply({
-            content: "No results were found!",
-          });
-
-        const embed = {
-          title: "Random Neko",
-          image: {
-            url: image,
-          },
-          color: color as ColorResolvable,
-        };
-
-        return message.channel.send({
-          embeds: [embed],
-        });
-      }
-      if (numberArray.includes(query)) {
-        if (args[1]) {
-          if (isNaN(+args[1]) || args[1].toLowerCase() === "infinity")
-            return randNumber(100);
-          return randNumber(+args[1]);
-        }
-        return randNumber(100);
-      }
-      message.reply({
-        embeds: [syntaxError],
+    if (!query)
+      return message.reply({
+        content: langs[lang].common.missing_arguments,
       });
 
+    if (waifuKeys.includes(query)) {
+      const image = await axios
+        .get("https://neko-love.xyz/api/v1/waifu", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/vnd.api+json",
+            Accept: "application/vnd.api+json",
+          },
+          responseType: "json",
+        })
+        .then((res) => res.data.url)
+        .catch((err) => {
+          message.reply({
+            content: langs[lang].common.no_results,
+          });
+          return;
+        });
+
+      if (!image)
+        return message.reply({
+          content: langs[lang].common.no_results,
+        });
+
+      const embed = {
+        title: langs[lang].modules.utility.random.waifu.embed.title,
+        image: {
+          url: image,
+        },
+        color: color as ColorResolvable,
+      };
+      return message.channel.send({
+        embeds: [embed],
+      });
+    }
+    if (nekoKeys.includes(query)) {
+      const image = await axios
+        .get("https://nekos.life/api/neko", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/vnd.api+json",
+            Accept: "application/vnd.api+json",
+          },
+          responseType: "json",
+        })
+        .then((res) => res.data.neko)
+        .catch(() => {
+          message.reply({
+            content: langs[lang].common.no_results,
+          });
+          return;
+        });
+
+      if (!image)
+        return message.reply({
+          content: langs[lang].common.no_results,
+        });
+      const embed = {
+        title: langs[lang].modules.utility.random.neko.embed.title,
+        image: {
+          url: image,
+        },
+        color: color as ColorResolvable,
+      };
+
+      return message.channel.send({
+        embeds: [embed],
+      });
+    }
+    if (numberKeys.includes(query)) {
+      if (!args[1] || (isNaN(+args[1]) && args[1].toLowerCase() !== "infinity"))
+        return randNumber(100);
+      return randNumber(+args[1]);
+
       function randNumber(max: number) {
-        const number = Math.round(Math.random() * max) + 0;
+        const number = Math.round(Math.random() * max);
         const embed = {
-          title: `Your random number is: ${number}`,
-          description: `Max: ${max}`,
+          title: langs[lang].modules.utility.random.number.embed.title(number),
+          description: `${langs[lang].common.max}: ${max}`,
           color: color as ColorResolvable,
         };
 
-        return message.channel.send({
+        message.channel.send({
           embeds: [embed],
         });
       }
-    } catch (err) {
-      console.log(err);
     }
+    await message.reply({
+      content: langs[lang].common.invalid_arguments,
+    });
   },
 });
-*/
